317651819e30c9f3acfe1b38cc64e45a5eb23b1a
Dania Juvale <71597487+Daniajuvale@users.noreply.github.com>
2022-12-03 22:03:08 +0530

    Initial commit

---
+++ views/home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+def app():
+
+    col1, col2 = st.columns([2,3])
+    with col1:
+        st.image("media/image.jpg")
+    with col2:
+        st.title("CUSTOMER LIFETIME VALUE MODELLING")
+        st.write("Know which customers are adding value to your store's lifetime!.")
+
+   
